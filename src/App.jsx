112d88698e661872bb6e3504a8d92051cdf3dca0
Atul Vinod <atul.v@keka.com>
2022-03-22 11:27:56 +0530

    Created category and category list component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import {Categories} from  './components';
+const App = () => {
+
+
+  return (
+    <div className="App">
+       <Categories/>
+    </div>
+  );
+};
+
+export default App;
