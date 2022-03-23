fd7f56f625c0ffa63f788f5393af24dc8001618a
Atul Vinod <atul.v@keka.com>
2022-03-23 09:50:55 +0530

    implemented categories component at home page

--- src/components/categories/categories.components.jsx
+++ src/components/categories/categories.components.jsx
@@ -33,7 +33,7 @@ export const Categories = () => {
   return (
     <div className="categories-container">
       {categories.map((category) => (
-        <CategoryItem category={category} />
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
