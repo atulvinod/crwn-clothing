162f33bfd517db5fe931265252701cfef684ab78
Atul Vinod <atul.v@keka.com>
2022-03-23 09:49:17 +0530

    Added nvaigation cpmponent

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import './navigation.styles.scss';
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
+export const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+              <CrwnLogo className="logo"/>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          <Link className="nav-link" to="/signin">
+            Sign-in
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
