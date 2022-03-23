19ea9997f4addbe61625a796e27baab02f80ff79
Atul Vinod <atul.v@keka.com>
2022-03-23 09:57:03 +0530

    Added firebase initial configuration

---
+++ src/services/auth/auth.service.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOqurrKCLfnz-gV9ItJfPdhpiu5Wj9XLk",
+    authDomain: "crwn-clothing-b9df2.firebaseapp.com",
+    projectId: "crwn-clothing-b9df2",
+    storageBucket: "crwn-clothing-b9df2.appspot.com",
+    messagingSenderId: "199746788477",
+    appId: "1:199746788477:web:0fe4241aef75229e39e514"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
