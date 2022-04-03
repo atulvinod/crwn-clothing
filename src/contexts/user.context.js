fe53b8779e1016ad71f9a1a29d2a76609a6ae561
Atul Vinod <atul.v@keka.com>
2022-04-03 10:47:59 +0530

    converted user-context to reducer

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -7,15 +7,45 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+export const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`unhandled type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+ /**
+  * In the reducer, when the dispatch function generates a new state, then the
+  * component is re-rendered
+  */
+  const { state, dispatch } = userReducer(userReducer, INITIAL_STATE);
+  const { currentUser } = state;
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
   const value = { currentUser, setCurrentUser };
 
-  useEffect(() =>{
-    const unsubscribe = onAuthStateChangedListener(({user}) =>{
-        // the listener will return the user when authenticated and 
-        // null when the user has logger out, hence we can centrailize this logic here
-        setCurrentUser(user);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener(({ user }) => {
+      // the listener will return the user when authenticated and
+      // null when the user has logger out, hence we can centrailize this logic here
+      setCurrentUser(user);
     });
     return unsubscribe;
   }, []);
